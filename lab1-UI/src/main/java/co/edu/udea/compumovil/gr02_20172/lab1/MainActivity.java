d45f38cfc2ddbaf003d89af8a78ce76034c4f720
raul.gomezr <rgr2228@gmail.com>
2017-08-18 20:22:14 -0500

    Se continuó con el laboratorio.

--- lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20172/lab1/MainActivity.java
+++ lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20172/lab1/MainActivity.java
@@ -1,9 +1,11 @@
 package co.edu.udea.compumovil.gr02_20172.lab1;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
