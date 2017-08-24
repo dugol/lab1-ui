bb58579d550961f17a7053198f3f68cfd1885cd5
raul.gomezr <rgr2228@gmail.com>
2017-08-24 12:34:32 -0500

    Se continuó el trabajo

--- lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20172/lab1/ProfileActivity.java
+++ lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20172/lab1/ProfileActivity.java
@@ -2,12 +2,31 @@ package co.edu.udea.compumovil.gr02_20172.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView firstNameText, lastNameText, genderText, birthdayText, phoneText, addressText, emailText, cityText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        firstNameText = (TextView) findViewById(R.id.first_name_info);
+        lastNameText = (TextView) findViewById(R.id.last_name_info);
+        genderText = (TextView) findViewById(R.id.gender_info);
+        birthdayText = (TextView) findViewById(R.id.birthday_info);
+        phoneText = (TextView) findViewById(R.id.phone_info);
+        addressText = (TextView) findViewById(R.id.address_info);
+        emailText = (TextView) findViewById(R.id.email_info);
+        cityText = (TextView) findViewById(R.id.city_info);
+        firstNameText.setText(getIntent().getStringExtra("KEY_FIRST_NAME"));
+        lastNameText.setText(getIntent().getStringExtra("KEY_LAST_NAME"));
+        genderText.setText(getIntent().getStringExtra("KEY_GENDER"));
+        birthdayText.setText(getIntent().getStringExtra("KEY_BIRTHDAY"));
+        phoneText.setText(getIntent().getStringExtra("KEY_PHONE"));
+        addressText.setText(getIntent().getStringExtra("KEY_ADDRESS"));
+        emailText.setText(getIntent().getStringExtra("KEY_EMAIL"));
+        cityText.setText(getIntent().getStringExtra("KEY_CITY"));
     }
 }
--- lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20172/lab1/SignUpActivity.java
+++ lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20172/lab1/SignUpActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
@@ -25,8 +27,11 @@ import static co.edu.udea.compumovil.gr02_20172.lab1.R.array.cities;
  * Created by user on 22/08/2017.
  */
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
-    private AutoCompleteTextView cityAutoComplete;
+public class SignUpActivity extends AppCompatActivity{
+    private EditText firstNameSign, lastNameSign, birthdaySign, phoneSign, addressSign, emailSign;
+    private RadioGroup genderSign;
+    private RadioButton genderSignOp;
+    private AutoCompleteTextView citySign;
     private EditText mEditInit;
     int year_x, month_x, day_x;
     static final int DILOG_ID=0;
@@ -35,9 +40,16 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_view);
+        firstNameSign = (EditText)findViewById(R.id.first_name);
+        lastNameSign = (EditText)findViewById(R.id.last_name);
+        birthdaySign = (EditText)findViewById(R.id.birthday);
+        phoneSign = (EditText)findViewById(R.id.phone);
+        addressSign = (EditText)findViewById(R.id.address);
+        emailSign = (EditText)findViewById(R.id.email);
         ArrayAdapter<String> citiesArray = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.cities));
-        cityAutoComplete = (AutoCompleteTextView)findViewById(R.id.city);
-        cityAutoComplete.setAdapter(citiesArray);
+        citySign = (AutoCompleteTextView)findViewById(R.id.city);
+        citySign.setAdapter(citiesArray);
+        genderSign = (RadioGroup)findViewById(R.id.genderRadio);
         /*PermissionListener permissionlistener = new PermissionListener() {
             @Override
             public void onPermissionGranted() {
@@ -67,19 +79,38 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         tedBottomPicker.show(getSupportFragmentManager());*/
     }
 
+    public void rbclick(View v)
+    {
+        int genderSignId = genderSign.getCheckedRadioButtonId();
+        genderSignOp = (RadioButton)findViewById(genderSignId);
+    }
 
-
-    @Override
+//firstNameSign, lastNameSign, birthdaySign, phoneSign, addressSign, emailSign
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.send:
-                Intent toMainActivity = new Intent(getApplicationContext(),SignUpActivity.class);
-                startActivity(toMainActivity);
+                if(!firstNameSign.getText().toString().equals(null) && !lastNameSign.getText().toString().equals(null) && !birthdaySign.getText().toString().equals(null) && !phoneSign.getText().toString().equals(null) && !addressSign.getText().toString().equals(null) && !emailSign.getText().toString().equals(null) && !citySign.getText().toString().equals(null) && !genderSignOp.getText().toString().equals(null)){
+                    Intent toProfile = new Intent(getApplicationContext(), ProfileActivity.class);
+                    toProfile.putExtra("KEY_FIRST_NAME", firstNameSign.getText().toString());
+                    toProfile.putExtra("KEY_LAST_NAME", lastNameSign.getText().toString());
+                    toProfile.putExtra("KEY_GENDER", genderSignOp.getText().toString());
+                    toProfile.putExtra("KEY_BIRTHDAY", birthdaySign.getText().toString());
+                    toProfile.putExtra("KEY_PHONE", phoneSign.getText().toString());
+                    toProfile.putExtra("KEY_ADDRESS", addressSign.getText().toString());
+                    toProfile.putExtra("KEY_EMAIL", emailSign.getText().toString());
+                    toProfile.putExtra("KEY_CITY", citySign.getText().toString());
+                    startActivity(toProfile);
+                }
+                else{
+                    Toast toast = Toast.makeText(getApplicationContext(),getString(R.string.warning2),Toast.LENGTH_SHORT);
+                    toast.show();
+                }
                 break;
 
             case R.id.birthday:
                 DialogFragment newFragment = new DatePickerFragment(view);
                 newFragment.show(getSupportFragmentManager(), "datePicker");
+                break;
         }
     }
 }
